0d6c855f6cfc1f1f7fc0faeb680614a2e70fe0e3
leandrocgsi <leandrocgsi@gmail.com>
2020-06-16 01:53:36 -0300

    Using Diferent Verbs GET, POST, PUT and DELETE

--- 03_APIAspNetCore5_UsingDiferentVerbs/APIAspNetCore5/Startup.cs
+++ 03_APIAspNetCore5_UsingDiferentVerbs/APIAspNetCore5/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using APIAspNetCore5.Services;
+using APIAspNetCore5.Services.Implementattions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +28,9 @@ namespace APIAspNetCore5
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImpl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
