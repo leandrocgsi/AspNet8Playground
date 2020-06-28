6e7eb2919f967b5209a78fb6cb363b223d1e81e1
leandrocgsi <leandrocgsi@gmail.com>
2020-06-28 00:12:20 -0300

    Working with Data Contract

---
+++ 10_APIAspNetCore5_DataContract/APIAspNetCore5/Data/VO/BookVO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace APIAspNetCore5.Data.VO
+{
+    [DataContract]
+    public class BookVO
+    {
+        [DataMember(Order = 1, Name = "codigo")]
+        public long? Id { get; set; }
+
+        [DataMember(Order = 2, Name = "titulo")]
+        public string Title { get; set; }
+
+        [DataMember(Order = 3, Name = "autor")]
+        public string Author { get; set; }
+
+        [DataMember(Order = 5, Name = "preco")]
+        public decimal Price { get; set; }
+
+        [DataMember(Order = 4, Name = "data")]
+        public DateTime LaunchDate { get; set; }
+    }
+}
