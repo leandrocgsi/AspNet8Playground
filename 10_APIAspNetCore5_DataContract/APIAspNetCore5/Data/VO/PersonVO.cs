d0124e5f1ed95c834f75e49fd3a3029a4abcad3b
leandrocgsi <leandrocgsi@gmail.com>
2020-06-28 20:21:15 -0300

    Custom JSON serialization

--- 10_APIAspNetCore5_DataContract/APIAspNetCore5/Data/VO/PersonVO.cs
+++ 10_APIAspNetCore5_DataContract/APIAspNetCore5/Data/VO/PersonVO.cs
@@ -1,11 +1,27 @@
-﻿namespace APIAspNetCore5.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace APIAspNetCore5.Data.VO
 {
     public class PersonVO
     {
+        [JsonPropertyName("code")]
         public long? Id { get; set; }
+
+        [JsonPropertyName("name")]
         public string FirstName { get; set; }
+
+
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
+
+
+        [JsonPropertyName("your_address")]
+        [JsonIgnore]
         public string Address { get; set; }
+
+
+        [JsonPropertyName("sex")]
         public string Gender { get; set; }
     }
 }
+//https://devblogs.microsoft.com/dotnet/try-the-new-system-text-json-apis/
