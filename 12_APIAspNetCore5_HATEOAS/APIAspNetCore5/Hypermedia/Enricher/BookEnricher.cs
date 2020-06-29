517f5ac97ff5e32b72861de7853073af2a2c71f7
leandrocgsi <leandrocgsi@gmail.com>
2020-06-29 16:17:54 -0300

    Implementing HATEOAS

---
+++ 12_APIAspNetCore5_HATEOAS/APIAspNetCore5/Hypermedia/Enricher/BookEnricher.cs
@@ -0,0 +1,54 @@
+﻿using APIAspNetCore5.Hypermedia.Constants;
+using APIAspNetCore5.Data.VO;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace APIAspNetCore5.Hypermedia.Enricher
+{
+    public class BookEnricher : ContentResponseEnricher<BookVO>
+    {
+        protected override Task EnrichModel(BookVO content, IUrlHelper urlHelper)
+        {
+            var path = "api/books/v1";
+            var url = new { controller = path, id = content.Id };
+
+            
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.GET,
+                Href = urlHelper.Link("DefaultApi", url),
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultGet
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.POST,
+                Href = urlHelper.Link("DefaultApi", url),
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultPost
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.PUT,
+                Href = urlHelper.Link("DefaultApi", url),
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultPost
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.PATCH,
+                Href = urlHelper.Link("DefaultApi", url),
+                Rel = RelationType.self,
+                Type = ResponseTypeFormat.DefaultPost
+            });
+            content.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.DELETE,
+                Href = urlHelper.Link("DefaultApi", url),
+                Rel = RelationType.self,
+                Type = "int"
+            });
+            return null;
+        }
+    }
+}
---
+++ 12_APIAspNetCore5_HATEOAS/APIAspNetCore5/Startup.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Net.Http.Headers;
+using APIAspNetCore5.Business;
+using APIAspNetCore5.Business.Implementations;
+using APIAspNetCore5.Model.Context;
+using APIAspNetCore5.Repository.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using APIAspNetCore5.Formatter;
+using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization;
+using APIAspNetCore5.Filters;
+using APIAspNetCore5.Hypermedia.Enricher;
+
+namespace APIAspNetCore5
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.PersonBusinessImplementation
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+            if (Environment.IsDevelopment())
+            {
+                MigrateDatabase(connection);
+            }
+
+            services.AddControllers();
+
+            var serializer = (Serializer) new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+            var deserializer = (Deserializer) new DeserializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+
+            services.AddMvc(options =>
+            {
+                //options.EnableEndpointRouting = false;
+                options.RespectBrowserAcceptHeader = true;
+
+                options.InputFormatters.Add(new YamlInputFormatter(deserializer));
+                options.OutputFormatters.Add(new YamlOutputFormatter(serializer));
+                options.FormatterMappings.SetMediaTypeMappingForFormat("yaml", MediaTypeHeaderValues.ApplicationYaml);
+
+                options.FormatterMappings.SetMediaTypeMappingForFormat("xml", MediaTypeHeaderValue.Parse("application/xml"));
+                options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
+
+            })
+            .AddXmlSerializerFormatters();
+
+            //Define as opções do filtro HATEOAS
+            var filterOptions = new HyperMediaFilterOptions();
+            filterOptions.ContentResponseEnricherList.Add(new PersonEnricher());
+            filterOptions.ContentResponseEnricherList.Add(new BookEnricher());
+            services.AddSingleton(filterOptions);
+
+            services.AddApiVersioning();
+
+            //Dependency Injection
+            services.AddScoped<IBookBusiness, BookBusinessImplementation>();
+            services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+            services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapControllerRoute("DefaultApi", "{controller=Values}/{id?}");
+            });
+        }
+
+        private static void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+                };
+
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed.", ex);
+                throw;
+            }
+        }
+
+    }
+}
