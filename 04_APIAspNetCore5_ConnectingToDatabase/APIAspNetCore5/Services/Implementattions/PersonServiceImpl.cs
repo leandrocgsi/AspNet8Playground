e6a79a94d66d0ed6b30603d2245df552ba5ea0d9
leandrocgsi <leandrocgsi@gmail.com>
2020-06-29 11:44:26 -0300

    Connecting to Database

--- 04_APIAspNetCore5_ConnectingToDatabase/APIAspNetCore5/Services/Implementattions/PersonServiceImpl.cs
+++ 04_APIAspNetCore5_ConnectingToDatabase/APIAspNetCore5/Services/Implementattions/PersonServiceImpl.cs
@@ -1,81 +1,100 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using APIAspNetCore5.Model;
+using APIAspNetCore5.Model.Context;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace APIAspNetCore5.Services.Implementattions
 {
     public class PersonServiceImpl : IPersonService
     {
 
-        // Contador responsável por gerar um fake ID já que não estamos
-        // acessando nenhum banco de dados
-        private volatile int count;
+        private MySQLContext _context;
+
+        public PersonServiceImpl(MySQLContext context)
+        {
+            _context = context;
+        }
 
         // Metodo responsável por criar uma nova pessoa
-        // Se tivéssemos um banco de dados esse seria o
-        // momento de persistir os dados
+        // nesse momento adicionamos o objeto ao contexto
+        // e finalmente salvamos as mudanças no contexto
+        // na base de dados
         public Person Create(Person person)
         {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
             return person;
         }
 
         // Método responsável por retornar uma pessoa
-        // como não acessamos nenhuma base de dados
-        // estamos retornando um mock
         public Person FindById(long id)
         {
-            return new Person
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        // Método responsável por retornar todas as pessoas
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        // Método responsável por atualizar uma pessoa
+        public Person Update(Person person)
+        {
+            // Verificamos se a pessoa existe na base
+            // Se não existir retornamos uma instancia vazia de pessoa
+            if (!Exists(person.Id)) return new Person();
+
+            // Pega o estado atual do registro no banco
+            // seta as alterações e salva
+            var result = _context.Persons.SingleOrDefault(b => b.Id == person.Id);
+            if (result != null)
             {
-                Id = IncrementAndGet(),
-                FirstName = "Leandro",
-                LastName = "Costa",
-                Address = "Uberlandia - Minas Gerais - Brasil",
-                Gender = "Male"
-            };
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return result;
         }
 
         // Método responsável por deletar
         // uma pessoa a partir de um ID
         public void Delete(long id)
         {
-            //A nossa lógica de exclusão viria aqui
-        }
-
-        // Método responsável por retornar todas as pessoas
-        // mais uma vez essas informações são mocks
-        public List<Person> FindAll()
-        {
-            List<Person> persons = new List<Person>();
-            for (int i = 0; i < 8; i++)
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            try
             {
-                Person person = MockPerson(i);
-                persons.Add(person);
+                if (result != null)
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
             }
-            return persons;
-        }
-
-        // Método responsável por atualizar uma pessoa
-        // por ser mock retornamos a mesma informação passada
-        public Person Update(Person person)
-        {
-            return person;
-        }
-
-        private Person MockPerson(int i)
-        {
-            return new Person
+            catch (Exception ex)
             {
-                Id = IncrementAndGet(),
-                FirstName = "Person Name " + i,
-                LastName = "Person Lastname " + i,
-                Address = "Some Address " + i,
-                Gender = "Male"
-            };
+                throw ex;
+            }
         }
 
-        private long IncrementAndGet()
+        private bool Exists(long? id)
         {
-            return Interlocked.Increment(ref count);
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
