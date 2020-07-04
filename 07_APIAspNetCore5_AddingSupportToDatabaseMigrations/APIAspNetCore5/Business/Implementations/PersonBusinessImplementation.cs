d3d9de12bc68fa4500e83a88f857982f5734a0fa
leandrocgsi <leandrocgsi@gmail.com>
2020-07-03 22:16:35 -0300

    Adding Support to Database Migrations

---
+++ 07_APIAspNetCore5_AddingSupportToDatabaseMigrations/APIAspNetCore5/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,47 @@
+﻿using APIAspNetCore5.Model;
+using APIAspNetCore5.Repository;
+using System.Collections.Generic;
+
+namespace APIAspNetCore5.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+
+        private readonly IPersonRepository _repository;
+
+        public PersonBusinessImplementation(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        public Person FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public bool Exists(long id)
+        {
+            return _repository.Exists(id);
+        }
+    }
+}
