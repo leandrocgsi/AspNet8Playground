6b6dad95c6a9e7250bd8f0ac836added2525e1ca
leandrocgsi <leandrocgsi@gmail.com>
2020-08-03 22:20:21 -0300

    Adding Support to Database Migrations

---
+++ 07_APIAspNetCore5_AddingSupportToDatabaseMigrations/APIAspNetCore5/Business/IPersonBusiness.cs
@@ -0,0 +1,17 @@
+﻿using APIAspNetCore5.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIAspNetCore5.Business
+{
+    public interface IPersonBusiness
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
