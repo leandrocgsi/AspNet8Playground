9b0eb23342f75f7d3039a8d2cb648e1648159622
leandrocgsi <leandrocgsi@gmail.com>
2020-08-19 11:45:18 -0300

    Working With Generic Repository

---
+++ 08_APIAspNetCore5_WorkingWithGenericRepository/APIAspNetCore5/Business/Implementations/BookBusinessImplementation.cs
@@ -0,0 +1,47 @@
+﻿using APIAspNetCore5.Model;
+using APIAspNetCore5.Repository.Generic;
+using System.Collections.Generic;
+
+namespace APIAspNetCore5.Business.Implementations
+{
+    public class BookBusinessImplementation : IBookBusiness
+    {
+
+        private IRepository<Book> _repository;
+
+        public BookBusinessImplementation(IRepository<Book> repository)
+        {
+            _repository = repository;
+        }
+
+        public Book Create(Book book)
+        {
+            return _repository.Create(book);
+        }
+
+        public Book FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        public List<Book> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Book Update(Book book)
+        {
+            return _repository.Update(book);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public bool Exists(long id)
+        {
+            return _repository.Exists(id);
+        }
+    }
+}
