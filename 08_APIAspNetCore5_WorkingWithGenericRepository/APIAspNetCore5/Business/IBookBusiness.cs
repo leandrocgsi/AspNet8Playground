029e6b4bf134ba1980158b6d41ea1a6ff4ae29c0
leandrocgsi <leandrocgsi@gmail.com>
2020-08-26 11:49:43 -0300

    Working With Generic Repository

---
+++ 08_APIAspNetCore5_WorkingWithGenericRepository/APIAspNetCore5/Business/IBookBusiness.cs
@@ -0,0 +1,14 @@
+﻿using APIAspNetCore5.Model;
+using System.Collections.Generic;
+
+namespace APIAspNetCore5.Business
+{
+    public interface IBookBusiness
+    {
+        Book Create(Book book);
+        Book FindById(long id);
+        List<Book> FindAll();
+        Book Update(Book book);
+        void Delete(long id);
+    }
+}
