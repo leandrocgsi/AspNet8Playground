b5a9e8a5c0478ed7b7c1c9d76e91ee37571efdfc
leandrocgsi <leandrocgsi@gmail.com>
2020-10-04 17:22:03 -0300

    Implementing HATEOAS

--- 12_APIAspNetCore5_HATEOAS/APIAspNetCore5/Hypermedia/Enricher/BookEnricher.cs
+++ 12_APIAspNetCore5_HATEOAS/APIAspNetCore5/Hypermedia/Enricher/BookEnricher.cs
@@ -12,12 +12,13 @@ namespace APIAspNetCore5.Hypermedia.Enricher
         protected override Task EnrichModel(BookVO content, IUrlHelper urlHelper)
         {
             var path = "api/books/v1";
-            string link = getLink(content, urlHelper, path);
+            string link = getLink(urlHelper, path);
+            string linkWithId = getLink(content, urlHelper, path);
 
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.GET,
-                Href = link,
+                Href = linkWithId,
                 Rel = RelationType.self,
                 Type = ResponseTypeFormat.DefaultGet
             });
@@ -31,27 +32,34 @@ namespace APIAspNetCore5.Hypermedia.Enricher
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.PUT,
-                Href = link,
+                Href = linkWithId,
                 Rel = RelationType.self,
                 Type = ResponseTypeFormat.DefaultPost
             });
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.PATCH,
-                Href = link,
+                Href = linkWithId,
                 Rel = RelationType.self,
                 Type = ResponseTypeFormat.DefaultPost
             });
             content.Links.Add(new HyperMediaLink()
             {
                 Action = HttpActionVerb.DELETE,
-                Href = link,
+                Href = linkWithId,
                 Rel = RelationType.self,
                 Type = "int"
             });
             return null;
         }
-
+        private string getLink(IUrlHelper urlHelper, string path)
+        {
+            lock (_lock)
+            {
+                var url = new { controller = path };
+                return new StringBuilder(urlHelper.Link("DefaultApi", url)).Replace("%2F", "/").ToString();
+            }
+        }
         private string getLink(BookVO content, IUrlHelper urlHelper, string path)
         {
             lock (_lock)
@@ -60,5 +68,6 @@ namespace APIAspNetCore5.Hypermedia.Enricher
                 return new StringBuilder(urlHelper.Link("DefaultApi", url)).Replace("%2F", "/").ToString();
             }
         }
+
     }
 }
