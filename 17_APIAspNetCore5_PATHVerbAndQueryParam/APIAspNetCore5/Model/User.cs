749e7b79b1a1db5a21b379144bb3019def483934
leandrocgsi <leandrocgsi@gmail.com>
2021-02-03 16:24:07 -0300

    Working with PATCH Verb And Query Param

--- 17_APIAspNetCore5_PATHVerbAndQueryParam/APIAspNetCore5/Model/User.cs
+++ 17_APIAspNetCore5_PATHVerbAndQueryParam/APIAspNetCore5/Model/User.cs
@@ -1,9 +1,17 @@
-﻿namespace APIAspNetCore5.Model
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIAspNetCore5.Model
 {
+    [Table("users")]
     public class User
     {
+        [Column("ID")]
         public long? Id { get; set; }
+
+        [Column("Login")]
         public string Login { get; set; }
+
+        [Column("AccessKey")]
         public string AccessKey { get; set; }
     }
 }
