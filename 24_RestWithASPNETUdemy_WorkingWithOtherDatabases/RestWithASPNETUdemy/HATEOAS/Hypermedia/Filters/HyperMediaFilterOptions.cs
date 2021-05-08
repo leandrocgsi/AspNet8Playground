f1a691d0d2a186427c414d5045decefd1e2a7fa9
leandrocgsi <leandrocgsi@gmail.com>
2021-05-08 15:51:02 -0300

    Working With Other Databases

---
+++ 24_RestWithASPNETUdemy_WorkingWithOtherDatabases/RestWithASPNETUdemy/HATEOAS/Hypermedia/Filters/HyperMediaFilterOptions.cs
@@ -0,0 +1,10 @@
+﻿using HATEOAS.Hypermedia.Abstract;
+using System.Collections.Generic;
+
+namespace HATEOAS.Hypermedia.Filters
+{
+    public class HyperMediaFilterOptions
+    {
+        public List<IResponseEnricher> ContentResponseEnricherList { get; set; } = new List<IResponseEnricher>();
+    }
+}
