ad8fb107ed683c19148044e4b0bf4b036208afc0
leandrocgsi <leandrocgsi@gmail.com>
2021-05-28 16:04:31 -0300

    Working With Other Databases

---
+++ 24_RestWithASPNETUdemy_WorkingWithOtherDatabases/RestWithASPNETUdemy/DataAccessMySqlProvider/Repository/Generic/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using DomainModel.Model.Base;
+using System.Collections.Generic;
+
+namespace DataAccessMySqlProvider.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+        T FindByID(long id);
+        List<T> FindAll();
+        T Update(T item);
+        void Delete(long id);
+        bool Exists(long id);
+
+        List<T> FindWithPagedSearch(string query);
+        int GetCount(string query);
+    }
+}
