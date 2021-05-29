79c058cc78eb545f20bde66c2497c1e502a7eede
leandrocgsi <leandrocgsi@gmail.com>
2021-05-29 16:04:53 -0300

    Working With Other Databases

---
+++ 24_RestWithASPNETUdemy_WorkingWithOtherDatabases/RestWithASPNETUdemy/DataAccessMySqlProvider/Repository/IPersonRepository.cs
@@ -0,0 +1,11 @@
+﻿using DomainModel.Model;
+using System.Collections.Generic;
+
+namespace DataAccessMySqlProvider.Repository
+{
+    public interface IPersonRepository : IRepository<Person>
+    {
+        Person Disable(long id);
+        List<Person> FindByName(string firstName, string secondName);
+    }
+}
