9e30b8f580dd0a4a478f6c940a8dc629608c637e
leandrocgsi <leandrocgsi@gmail.com>
2021-06-08 16:08:58 -0300

    Working With Other Databases

---
+++ 24_RestWithASPNETUdemy_WorkingWithOtherDatabases/RestWithASPNETUdemy/HATEOAS/Hypermedia/Abstract/ISupportsHyperMedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace HATEOAS.Hypermedia.Abstract
+{
+    public interface ISupportsHyperMedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
