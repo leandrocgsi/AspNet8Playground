0905f85e33ec1db294e82cac2b2d6a40b8ffbcf6
leandrocgsi <leandrocgsi@gmail.com>
2021-08-15 14:20:09 -0300

    Working with other databases - Postgres

---
+++ 25_RestWithASPNETUdemy_Postgres/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Context/PostgreeSQLContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestWithASPNETUdemy.Model.Context
+{
+    public class PostgreeSQLContext : DbContext
+    {
+        public PostgreeSQLContext()
+        {
+        
+        }
+        public PostgreeSQLContext(DbContextOptions<PostgreeSQLContext> options) : base(options) {}
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
