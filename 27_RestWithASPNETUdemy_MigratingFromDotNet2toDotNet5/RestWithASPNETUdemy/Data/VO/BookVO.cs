f7fe7735c2f8580183551dd42d6d99fe39c7ea85
leandrocgsi <leandrocgsi@gmail.com>
2021-12-12 15:27:45 -0300

    Migrating from .NET 2.2 to .NET 5

---
+++ 27_RestWithASPNETUdemy_MigratingFromDotNet2toDotNet5/RestWithASPNETUdemy/Data/VO/BookVO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using RestWithASPNETUdemy.Hypermedia.Abstract;
+using RestWithASPNETUdemy.Hypermedia;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class BookVO : ISupportsHyperMedia
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public DateTime LaunchDate { get; set; }
+
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
