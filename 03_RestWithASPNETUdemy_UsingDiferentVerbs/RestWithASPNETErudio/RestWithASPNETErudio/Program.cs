6c283590185e149451d138b73819a3449427901a
leandrocgsi <leandrocgsi@gmail.com>
2023-11-15 11:43:02 -0300

    Working with four main Verbs

---
+++ 03_RestWithASPNETUdemy_UsingDiferentVerbs/RestWithASPNETErudio/RestWithASPNETErudio/Program.cs
@@ -0,0 +1,32 @@
+using RestWithASPNETErudio.Services.Implementations;
+using RestWithASPNETErudio.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
